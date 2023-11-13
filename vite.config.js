585ec2cae2144a153a23ac1163d754ec161ed4f3
abhay395 <abhayparja90@gmail.com>
2023-11-13 13:14:30 +0530

    cl

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer'), // Add Autoprefixer
-        // You can add other PostCSS plugins here
-      ],
-    },
-  },
-});
+  plugins: [react()],
+})
