e09b93366f4115b512d5ebedc964fd05f02dc67b
abhay395 <abhayparja90@gmail.com>
2023-11-14 11:41:33 +0530

    c

--- src/Header/SingleProduct.jsx
+++ src/Header/SingleProduct.jsx
@@ -13,27 +13,30 @@ function SingleProduct() {
   const { name } = useParams();
   let [index, setindex] = useState(0);
   const authstatus = useSelector((state) => state.status);
-  // const AllProducts = useSelector((state) => state.Categorys[1]);
-  // const filterProduct = AllProducts
-  //   ? AllProducts.filter((item) => item.id == Number(name))
-  //   : [];
-  const [filterProduct, setfilterProduct] = useState([]);
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/1")
-      .then((res) => res.json())
-      .then((resp)=>setfilterProduct(resp))
-      .catch((error)=>console.log(error))
-  }, [name]);
-  const ideaof = filterProduct.length ? useSelector((state) => state.Addedid) : [];
+  const AllProducts = useSelector((state) => state.Categorys[1]);
+  const filterProduct = AllProducts
+    ? AllProducts.filter((item) => item.id == Number(name))
+    : [];
+  const ideaof = filterProduct ? useSelector((state) => state.Addedid) : [];
   const activity =
-    ideaof.length != 0 ? ideaof.includes(filterProduct.id) : false;
- 
+    ideaof.length != 0 ? ideaof.includes(filterProduct[0].id) : false;
+  // window.addEventListener("beforeunload", function (e) {
+  //   // Cancel the event
+  //   e.preventDefault();
+  //   // Chrome requires returnValue to be set
+  //   e.returnValue = "";
+
+  //   // Display a confirmation message to the user
+  //   const confirmationMessage = "Leaving this page will result in data loss.";
+  //   e.returnValue = confirmationMessage;
+  //   return confirmationMessage;
+  // });
   useEffect(() => {
     if (filterProduct.length != 0) {
       if (index < 0) {
-        setindex(filterProduct.images.length - 1);
+        setindex(filterProduct[0].images.length - 1);
       }
-      if (index > filterProduct.images.length - 1) {
+      if (index > filterProduct[0].images.length - 1) {
         setindex(0);
         // if(index===0) setindex(0)
       }
@@ -42,12 +45,12 @@ function SingleProduct() {
 
   const [quantity, setquantity] = useState(1);
   if (filterProduct.length != 0)
-    console.log(typeof filterProduct.images.length);
+    console.log(typeof filterProduct[0].images.length);
   if (filterProduct.length != 0) {
     return (
       <section className="flex flex-col " id="CategoryItems">
         <section id="section">
-          {filterProduct.images.map((item, imageIndex) => {
+          {filterProduct[0].images.map((item, imageIndex) => {
             let postion = "nextSlide";
             // console.log(imageIndex, index);
             if (imageIndex === index) {
@@ -55,7 +58,7 @@ function SingleProduct() {
             }
             if (
               imageIndex === index - 1 ||
-              (index === 0 && imageIndex === filterProduct.images.length - 1)
+              (index === 0 && imageIndex === filterProduct[0].images.length - 1)
             ) {
               postion = "lastSlide";
             }
@@ -74,19 +77,19 @@ function SingleProduct() {
         </section>
         <div className="w-[300px] relative ">
           <div className="absolute top-[10px]">
-            <h1 className="font-bold text-[30px]">{filterProduct.title}</h1>
+            <h1 className="font-bold text-[30px]">{filterProduct[0].title}</h1>
             <h1 className="text-yellow-500 text-[20px] ">
               &#9733;&#9733;&#9733;&#9733;
               <span className="text-[15px] text-black">
-                {filterProduct.rating}k
+                {filterProduct[0].rating}k
               </span>
             </h1>
-            <h2 className="font-semibold text-sky-600 text-[25px]">{`${filterProduct.price}$`}</h2>
+            <h2 className="font-semibold text-sky-600 text-[25px]">{`${filterProduct[0].price}$`}</h2>
             <h2 className="text-[15px] opacity-50  ">
-              {filterProduct.description}
+              {filterProduct[0].description}
             </h2>
             <h4 className="text-[#FF0000] font-bold text-[12px]">
-              Only {filterProduct.stock} left{" "}
+              Only {filterProduct[0].stock} left{" "}
             </h4>
            
             <div className="flex   ">
@@ -102,10 +105,10 @@ function SingleProduct() {
                 onClick={() => {
                   if (authstatus) {
                     if (!activity) {
-                      dispatch(addedid(filterProduct.id));
+                      dispatch(addedid(filterProduct[0].id));
                       dispatch(
                         addCart([
-                          filterProduct,
+                          filterProduct[0],
                           { quantityid: Number(quantity) },
                         ])
                       );
