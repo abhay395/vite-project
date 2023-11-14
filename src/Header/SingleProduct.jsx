1bf4b9fa07f240642a8178417e75ce48a35fab22
abhay395 <abhayparja90@gmail.com>
2023-11-14 16:15:20 +0530

    cm

--- src/Header/SingleProduct.jsx
+++ src/Header/SingleProduct.jsx
@@ -6,134 +6,100 @@ import { addCart, addedid } from "../Store/CategorySlice";
 import { useNavigate } from "react-router-dom";
 
 function SingleProduct() {
-  // const [data ,setData]= useState(null)
-  const [Loading ,setLoading] = useState(false)
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { name } = useParams();
-  let [index, setindex] = useState(0);
-  const authstatus = useSelector((state) => state.status);
-  const AllProducts = useSelector((state) => state.Categorys[1]);
-  const filterProduct = AllProducts
-    ? AllProducts.filter((item) => item.id == Number(name))
-    : [];
-  const ideaof = filterProduct ? useSelector((state) => state.Addedid) : [];
-  const activity =
-    ideaof.length != 0 ? ideaof.includes(filterProduct[0].id) : false;
-  // window.addEventListener("beforeunload", function (e) {
-  //   // Cancel the event
-  //   e.preventDefault();
-  //   // Chrome requires returnValue to be set
-  //   e.returnValue = "";
+  const [index, setIndex] = useState(0);
+  const authStatus = useSelector((state) => state.status);
+  const allProducts = useSelector((state) => state.Categorys[1]);
+  const filterProduct = allProducts?.find(item => item.id === Number(name)) || {};
 
-  //   // Display a confirmation message to the user
-  //   const confirmationMessage = "Leaving this page will result in data loss.";
-  //   e.returnValue = confirmationMessage;
-  //   return confirmationMessage;
-  // });
+  const ideaOf = useSelector((state) => state.Addedid) || [];
+  const activity = ideaOf.includes(filterProduct.id);
+
   useEffect(() => {
-    if (filterProduct.length != 0) {
-      if (index < 0) {
-        setindex(filterProduct[0].images.length - 1);
-      }
-      if (index > filterProduct[0].images.length - 1) {
-        setindex(0);
-        // if(index===0) setindex(0)
-      }
+    if (filterProduct.images?.length && index < 0) {
+      setIndex(filterProduct.images.length - 1);
+    } else if (filterProduct.images?.length && index > filterProduct.images.length - 1) {
+      setIndex(0);
     }
-    const  timout = setTimeout(()=>{setLoading(true)},2000)
-    return ()=>clearTimeout(timout)
-  }, [index]);
+  }, [index, filterProduct]);
 
-  const [quantity, setquantity] = useState(1);
-  if (filterProduct.length != 0)
-    console.log(typeof filterProduct[0].images.length);
-  if (filterProduct.length != 0 && Loading) {
-    return (
-      <section className="flex flex-col " id="CategoryItems">
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAddToCart = () => {
+    if (authStatus) {
+      if (!activity) {
+        dispatch(addedid(filterProduct.id));
+        dispatch(addCart([filterProduct, { quantityid: Number(quantity) }]));
+      }
+      navigate("/cart");
+    } else {
+      navigate('/login');
+    }
+  };
+
+  return (
+    <section className="flex flex-col" id="CategoryItems">
+      {filterProduct.images && (
         <section id="section">
-          {filterProduct[0].images.map((item, imageIndex) => {
-            let postion = "nextSlide";
-            // console.log(imageIndex, index);
+          {filterProduct.images.map((item, imageIndex) => {
+            let position = "nextSlide";
             if (imageIndex === index) {
-              postion = "activeSlide";
+              position = "activeSlide";
             }
             if (
               imageIndex === index - 1 ||
-              (index === 0 && imageIndex === filterProduct[0].images.length - 1)
+              (index === 0 && imageIndex === filterProduct.images.length - 1)
             ) {
-              postion = "lastSlide";
+              position = "lastSlide";
             }
             return (
-              <article id={postion} key={imageIndex}>
+              <article id={position} key={imageIndex}>
                 <img src={`${item}`} className="image" alt="" />
               </article>
             );
           })}
-          <button className="prev" onClick={() => setindex(index - 1)}>
+          <button className="prev" onClick={() => setIndex(index - 1)}>
             <FiChevronLeft />
           </button>
-          <button className="next" onClick={() => setindex(index + 1)}>
+          <button className="next" onClick={() => setIndex(index + 1)}>
             <FiChevronRight />
           </button>
         </section>
-        <div className="w-[300px] relative ">
-          <div className="absolute top-[10px]">
-            <h1 className="font-bold text-[30px]">{filterProduct[0].title}</h1>
-            <h1 className="text-yellow-500 text-[20px] ">
-              &#9733;&#9733;&#9733;&#9733;
-              <span className="text-[15px] text-black">
-                {filterProduct[0].rating}k
-              </span>
-            </h1>
-            <h2 className="font-semibold text-sky-600 text-[25px]">{`${filterProduct[0].price}$`}</h2>
-            <h2 className="text-[15px] opacity-50  ">
-              {filterProduct[0].description}
-            </h2>
-            <h4 className="text-[#FF0000] font-bold text-[12px]">
-              Only {filterProduct[0].stock} left{" "}
-            </h4>
-           
-            <div className="flex   ">
-              <button className="bg-sky-400 rounded-lg p-2 text-[12px] w-[80px] h-[35px]  ">Bay Now</button>
-              <input
-                type="number"
-                onChange={(e) => setquantity(e.currentTarget.value)}
-                value={quantity}
-                className="w-[40px]  p-1 m-1"
-              />
-              <button
-                className="bg-yellow-400 rounded-lg p-2 ml-3 text-[12px] w-[80px] h-[35px] "
-                onClick={() => {
-                  if (authstatus) {
-                    if (!activity) {
-                      dispatch(addedid(filterProduct[0].id));
-                      dispatch(
-                        addCart([
-                          filterProduct[0],
-                          { quantityid: Number(quantity) },
-                        ])
-                      );
-                    }
-                    navigate("/cart");
-                  }
-                  else{
-                    navigate('/login')
-                  }
-                }}
-              >
-                Add to Cart
-              </button>
-            </div>
+      )}
+      <div className="w-[300px] relative ">
+        <div className="absolute top-[10px]">
+          <h1 className="font-bold text-[30px]">{filterProduct.title}</h1>
+          <h1 className="text-yellow-500 text-[20px]">
+            &#9733;&#9733;&#9733;&#9733;
+            <span className="text-[15px] text-black">{filterProduct.rating}k</span>
+          </h1>
+          <h2 className="font-semibold text-sky-600 text-[25px]">{`${filterProduct.price}$`}</h2>
+          <h2 className="text-[15px] opacity-50">{filterProduct.description}</h2>
+          <h4 className="text-[#FF0000] font-bold text-[12px]">
+            Only {filterProduct.stock} left
+          </h4>
+          <div className="flex">
+            <button className="bg-sky-400 rounded-lg p-2 text-[12px] w-[80px] h-[35px]">
+              Buy Now
+            </button>
+            <input
+              type="number"
+              onChange={(e) => setQuantity(e.currentTarget.value)}
+              value={quantity}
+              className="w-[40px] p-1 m-1"
+            />
+            <button
+              className="bg-yellow-400 rounded-lg p-2 ml-3 text-[12px] w-[80px] h-[35px]"
+              onClick={handleAddToCart}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
-      </section>
-    );
-  }
- if(!Loading) return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <span className="loader w-[100px] h-[100px] text-center "></span>
-    </div>
+      </div>
+    </section>
   );
 }
 
