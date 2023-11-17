393e1a07d693d08c92b513d2e994ac09515a37a2
abhay395 <abhayparja90@gmail.com>
2023-11-17 16:18:17 +0530

    cc

--- src/Header/Items.jsx
+++ src/Header/Items.jsx
@@ -22,15 +22,15 @@ function Items({ Category }) {
     if (!Loading)  return (
         <div
           id="CategoryItems"
-          className=" flex flex-col justify-evenly sm:flex-row sm:flex-wrap w-full  space-y-3 space-x-2 items-center transition-all ease-in-out delay-500"
+          className=" flex flex-col justify-evenly sm:flex-row sm:flex-wrap w-full mt-4  space-y-3 border space-x-2 items-center transition-all ease-in-out delay-500"
         >
           {filterProduct.map((item) => (
             <div
               key={item.id}
               id="CategoryItems"
-              className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px] "
+              className="bg-white border rounded-sm  flex p-2 overflow-hidden shadow-2xl h-[200px] w-full   sm:flex-col sm:w-[250px] sm:h-[350px] "
             >
-              <div className="w-[150px] sm:w-full sm:h-[150px] h-full ">
+              <div className="w-[150px] sm:w-full sm:h-[150px] h-full border ">
                 <Link to={`/al/${item.id}`}>
                   <img
                     src={
@@ -60,7 +60,7 @@ function Items({ Category }) {
         
       );
        return (
-        <div className="flex flex-col justify-evenly sm:flex-row sm:flex-wrap space-y-3 space-x-2 items-center transition-all ease-in-out delay-500 w-[95%]  ">
+        <div className="flex flex-col justify-evenly sm:flex-row sm:flex-wrap space-y-3 mt-4 space-x-2 items-center transition-all ease-in-out delay-500 w-[95%]  ">
           <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px] "></div>
           <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px]"></div>
           <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px] "></div>
@@ -71,7 +71,7 @@ function Items({ Category }) {
   }
  
   return (
-    <div className="flex flex-col justify-evenly sm:flex-row sm:flex-wrap space-y-3 space-x-2 items-center transition-all ease-in-out delay-500 w-[95%]  ">
+    <div className="flex flex-col justify-evenly sm:flex-row sm:flex-wrap space-y-3 mt-4 space-x-2 items-center transition-all ease-in-out delay-500 w-[95%]  ">
       <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px] "></div>
       <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px]"></div>
       <div className="bg-white border rounded-sm border-white flex p-2 overflow-hidden shadow-2xl h-[200px] w-[100%]  sm:flex-col sm:w-[250px] sm:h-[350px] "></div>
